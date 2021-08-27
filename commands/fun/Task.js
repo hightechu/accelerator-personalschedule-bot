0ced65ea0f4600c722ca960e4cece4d8d5c5cb54
AngusChou <Naraka9978@gmail.com>
2021-08-28 04:04:07 +0800

    Add a reminder system for the task command

--- commands/fun/Task.js
+++ commands/fun/Task.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 // Ping Module
 module.exports = {
     // Name of Command
@@ -16,5 +17,49 @@ module.exports = {
         let amount = args[0].split('-');
         // Send Message
         message.channel.send("Okay, I have added "+ amount[0] + " in the schedule and have assigned it for "+ amount[1] + " minutes.");
+
+        // Setup Reminder
+        let startDate = Date().toString();
+        console.log(`Starting reminder messages at ${startDate}`);
+
+        // Setup TimeInterval
+        let timeInterval = 1;
+
+        // Send Confirmation
+        message.channel.send('Reminder has been set!');
+        
+        const r = ['A simple method to prevent eye strain during long periods of screentime is the 20-20-20 rule.  Every 20 minutes you should look at something 20 feet away from you for a total of 20 seconds.', ' Just a reminder to stay hydrated so that we can stay healthy!', 'remember to do some exercises every once in a while to stay fit.'];
+        
+        let old = 55;
+        // Start Reminder - Every X Minutes
+        let interval = setInterval(function () {
+            
+        let randomize = (Math.floor(Math.random()*(3-1+1)+1))-1;
+        
+        while (randomize == old){
+        randomize = (Math.floor(Math.random()*(3-1+1)+1))-1;
+
+        
+        }
+
+        old = randomize;   
+        
+            // Create Embed
+            const embed = new Discord.MessageEmbed()
+                .setColor('FFE3DC')
+                .setTitle('Reminder!')
+                .setDescription(`${message.author}, this is a reminder!`)
+                
+                .addFields(
+                    
+                    
+                    {name: 'Health!', value: r[randomize]})
+                .setTimestamp()
+                .setFooter('Powered by PSB Discord Bot');
+
+            // Send Message
+            message.author.send(embed);
+        }, timeInterval * 60 * 1 * 1000);
+    
     },
 };
