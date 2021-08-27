7d85069a60cce8ea06c229cbbae691fc89e2b76f
AngusChou <Naraka9978@gmail.com>
2021-08-28 04:30:55 +0800

    change the reminder system for the task command

--- commands/fun/Calender.js
+++ commands/fun/Calender.js
@@ -22,12 +22,12 @@ module.exports = {
                 { name: '\u200B', value: '\u200B' },
                 { name: 'Monday', value: 'A : 6:30pm - 7:00pm', inline: true },
                 { name: 'Tuesday', value: 'No Task Today!', inline: true },
-                { name: 'Wednesday', value: 'B : 2pm - 3pm', inline: true },
-                { name: 'Thursday', value: 'C : 2pm - 2:45pm, D : 4:45pm - 6pm', inline: true },
+                { name: 'Wednesday', value: 'B : 2pm - 2:30pm', inline: true },
+                { name: 'Thursday', value: 'No task today!', inline: true },
                 { name: 'Friday', value: 'No Task Today!', inline: true },
-                { name: 'Saturday', value: 'E : 11am - 1pm', inline: true },
-                { name: 'Sunday', value: 'F : 3pm - 7pm', inline: true },
-                { name: 'Task Amount for this week', value: '6', inline: true },
+                { name: 'Saturday', value: 'C: 5pm - 5:30pm', inline: true },
+                { name: 'Sunday', value: 'No Task Today!', inline: true },
+                { name: 'Task Amount for this week', value: '3', inline: true },
             )
             .setTimestamp()
             .setFooter('Powered by UVic Engineering', 'https://hightechu.ca/wp-content/uploads/2019/09/logo-2-150x150.png');
--- commands/fun/taskRemind.js
+++ commands/fun/taskRemind.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 // Ping Module
 module.exports = {
     // Name of Command
-    name: 'task',
+    name: 'taskR',
     // Description of Command
     description: 'Assign the task to the corresponding letter and assign the time (minutes).',
     usage: '[l-mm]',
@@ -18,6 +18,31 @@ module.exports = {
         // Send Message
         message.channel.send("Okay, I have added "+ amount[0] + " in the schedule and have assigned it for "+ amount[1] + " minutes.");
 
+        // Setup Reminder
+        let startDate = Date().toString();
+        console.log(`Starting reminder messages at ${startDate}`);
+
+        // Setup TimeInterval
+        let timeInterval = 1;
+
+       
+       
+        
+        const r = ['A simple method to prevent eye strain during long periods of screentime is the 20-20-20 rule.  Every 20 minutes you should look at something 20 feet away from you for a total of 20 seconds.', ' Just a reminder to stay hydrated so that we can stay healthy!', 'remember to do some exercises every once in a while to stay fit.'];
+        
+        let old = 55;
+        // Start Reminder - Every X Minutes
+        let interval = setInterval(function () {
+            
+        let randomize = (Math.floor(Math.random()*(3-1+1)+1))-1;
+        
+        while (randomize == old){
+        randomize = (Math.floor(Math.random()*(3-1+1)+1))-1;
+
+        
+        }
+
+        old = randomize;   
         
             // Create Embed
             const embed = new Discord.MessageEmbed()
@@ -28,12 +53,13 @@ module.exports = {
                 .addFields(
                     
                     
-                    {name: 'Health!', value:"A simple method to prevent eye strain during long periods of screentime is the 20-20-20 rule.  Every 20 minutes you should look at something 20 feet away from you for a total of 20 seconds."})
+                    {name: 'Health!', value: r[randomize]})
                 .setTimestamp()
                 .setFooter('Powered by PSB Discord Bot');
 
             // Send Message
             message.author.send(embed);
-        }
+        }, timeInterval * 60 * 1 * 1000);
     
-    }
+    },
+};
